28642bb719e20de2e2c5dfff97354b26851d71ee
neslondiaze <neslondiaze@gmail.ccom>
2020-05-15 21:44:18 -0400

    fin configuracon handlebars

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const path = require('path');
 
 //TODO: Inicializacion 
@@ -8,7 +11,14 @@ const app = express();
 //TODO:Configuracion
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
-
+app.engine('.hbs', expressHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 //TODO: Middelawares
 app.use(express.urlencoded({extended: false}));
@@ -20,7 +30,7 @@ app.use(express.urlencoded({extended: false}));
 
 //TODO: Routes
 app.get('/', (req, res) => {
-    res.send('Hola estoy corriendo');
+    res.render('index');
 });
 
 
